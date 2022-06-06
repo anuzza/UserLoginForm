a1dad988d65ae9827ff82ad7799fa49d6f036306
anuzza <anuja3580@gmail.com>
2022-06-06 11:24:17 -0500

    useReducer Hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,55 +1,86 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import { act } from '@testing-library/react';
+
+const emailReducer = (state, action )=>{
+  if(action.type==='USER_INPUT'){
+    return{value: action.val, isValid: action.val.includes('@')};
+  }
+
+  if(action.type === 'INPUT_BLUR'){
+    return {value: state.value, isValid: state.value.includes('@')};
+  }
+
+return {value: '', isValid: false};
+
+};  
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '', 
+    isValid: null,
+  });
+
    
-  // useEffect (()=>{
-  //   console.log('EFFECT RUNNING');
-  //   return()=>{
-  //     console.log('EFFECT CLEANUP');
-  //   }
-  // }, []);
-
-  //debouncing
   useEffect (()=>{
-    const identifier = setTimeout(() => {
-      console.log('checking for validity');
-      setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-      
-    }, 500);
-    return ()=>{
-      console.log('cleanup');
-      clearTimeout(identifier);
+    console.log('EFFECT RUNNING');
+    return()=>{
+      console.log('EFFECT CLEANUP');
+    }
+  }, []);
 
-    };
-  }, [enteredEmail, enteredPassword]);
+
+  // //debouncing
+  // useEffect (()=>{
+  //   const identifier = setTimeout(() => {
+  //     console.log('checking for validity');
+  //     setFormIsValid(
+  //     enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //   );
+      
+  //   }, 500);
+  //   return ()=>{
+  //     console.log('cleanup');
+  //     clearTimeout(identifier);
+
+  //   };
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({type: 'USER_INPUT',
+     val: event.target.value,
+    });
 
-    
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    );
+      
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
+  
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    );
+      
 
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    //setEmailIsValid(emailState.isValid);
+    dispatchEmail({type: 'INUPT_BLUR'});
   };
+  
 
   const validatePasswordHandler = () => {
     setPasswordIsValid(enteredPassword.trim().length > 6);
@@ -57,7 +88,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -65,14 +96,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
@@ -100,5 +131,6 @@ const Login = (props) => {
     </Card>
   );
 };
+
 
 export default Login;
